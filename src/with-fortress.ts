ac986666e52e1fce7671bad101379a328a11cbc7
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-07-23 17:05:51 +0900

    update src/with-fortress.ts

--- src/with-fortress.ts
+++ src/with-fortress.ts
@@ -1,3 +1,46 @@
-export const withFortress = () => (): void => {
-  // write after
+import { NextConfig } from 'next/dist/next-server/server/config-shared'
+
+type InspectByIp = {
+  inspectBy: 'ip'
+  ips: string | Array<string>
 }
+
+// WIP
+type InspectByCookie = {
+  inspectBy: 'cookie'
+}
+
+// WIP
+type InspectByHeader = {
+  inspectBy: 'header'
+}
+
+type Block = {
+  mode: 'block'
+  statusCode?: number
+}
+
+type Redirect = {
+  mode: 'redirect'
+  statusCode?: number
+  destination: string
+}
+
+type Rewrite = {
+  mode: 'rewrite'
+  destination: string
+}
+
+type Fort = {
+  source: string
+} & (Block | Redirect | Rewrite) &
+  (InspectByIp | InspectByCookie | InspectByHeader)
+
+export const withFortress =
+  (forts: Fort[]) =>
+  (config: Partial<NextConfig>): Partial<NextConfig> => {
+    console.log(forts)
+    return {
+      ...config
+    }
+  }
