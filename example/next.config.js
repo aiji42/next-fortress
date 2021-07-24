faa26031b174fcd0533628644dc628d568f5260d
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-07-24 23:46:22 +0900

    chore: update example

--- example/next.config.js
+++ example/next.config.js
@@ -1,31 +1,34 @@
-const withFortress = require('next-fortress')([
-  {
-    source: '/foo/redirect',
-    inspectBy: 'ip',
-    mode: 'redirect',
-    destination: '/',
-    ips: ["14.11.11.224"]
-  },
-  {
-    source: '/foo/block',
-    inspectBy: 'ip',
-    mode: 'block',
-    ips: ["14.11.11.224"]
-  },
-  {
-    source: '/foo/redirect2',
-    inspectBy: 'ip',
-    mode: 'redirect',
-    destination: '/',
-    ips: ["14.11.11.225"]
-  },
-  {
-    source: '/foo/block2',
-    inspectBy: 'ip',
-    mode: 'block',
-    ips: ["14.11.11.225"]
-  }
-])
+const withFortress = require('./next-fortress')({
+  forts: [
+    {
+      source: '/foo/redirect',
+      inspectBy: 'ip',
+      mode: 'redirect',
+      destination: '/',
+      ips: ["14.11.11.224"]
+    },
+    {
+      source: '/foo/block',
+      inspectBy: 'ip',
+      mode: 'block',
+      ips: ["14.11.11.224"]
+    },
+    {
+      source: '/foo/redirect2',
+      inspectBy: 'ip',
+      mode: 'redirect',
+      destination: '/',
+      ips: ["14.11.11.225"]
+    },
+    {
+      source: '/foo/block2',
+      inspectBy: 'ip',
+      mode: 'block',
+      ips: ["14.11.11.225"]
+    }
+  ],
+  host: process.env.VERCEL_URL ?? 'http://localhost:3000'
+})
 
 module.exports = withFortress({
   reactStrictMode: true,
