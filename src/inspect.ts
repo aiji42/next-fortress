ceed50c15abf275adc14897feff6364ea42f0ec7
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-07-24 21:32:12 +0900

    update inspect

--- src/inspect.ts
+++ src/inspect.ts
@@ -2,6 +2,22 @@ import { GetServerSideProps, GetServerSidePropsContext } from 'next'
 import getConfig from 'next/config'
 import { Fort } from './with-fortress'
 import { reverseProxy } from './reverse-proxy'
+import ipCidr from 'ip-cidr'
+
+export const inspectIp = (
+  ips: string | string[],
+  target: string | string[] | undefined
+): boolean => {
+  return (Array.isArray(ips) ? ips : [ips]).some((ip) => {
+    if (!ipCidr.isValidAddress(ip)) return false
+    const ipAddress = ipCidr.createAddress(ip)
+    return new ipCidr(
+      `${ipAddress.addressMinusSuffix}${ipAddress.subnet}`
+    ).contains(
+      Array.isArray(target) ? target[0] : target ? target.split(',')[0] : target
+    )
+  })
+}
 
 export const runReverseProxy = async ({
   req,
@@ -22,31 +38,38 @@ export const runReverseProxy = async ({
   )
 }
 
+const emptyProps = {
+  props: {}
+}
+
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const config: Fort =
     getConfig().serverRuntimeConfig.forts[<string>ctx.query.__key]
 
   console.log(config)
 
-  if (config.mode === 'redirect')
-    return {
-      redirect: {
-        destination: config.destination,
-        statusCode: config.statusCode ?? 301
+  let allow = false
+  if (config.inspectBy === 'ip' && ctx.req.headers['x-forwarded-for']) {
+    allow = inspectIp(config.ips, ctx.req.headers['x-forwarded-for'])
+  }
+
+  if (!allow) {
+    if (config.mode === 'redirect')
+      return {
+        redirect: {
+          destination: config.destination,
+          statusCode: config.statusCode ?? 301
+        }
       }
-    }
 
-  if (config.mode === 'block') {
-    ctx.res.writeHead(config.statusCode ?? 400)
-    ctx.res.end()
-    return {
-      props: {}
+    if (config.mode === 'block') {
+      ctx.res.writeHead(config.statusCode ?? 400)
+      ctx.res.end()
+      return emptyProps
     }
+  } else {
+    await runReverseProxy(ctx)
   }
 
-  await runReverseProxy(ctx)
-
-  return {
-    props: {}
-  }
+  return emptyProps
 }
