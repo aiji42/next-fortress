24b6ca04bb98d59ee09a6511b0b49b294c8358d0
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-07-24 21:58:03 +0900

    chore: update example

--- example/next.config.js
+++ example/next.config.js
@@ -4,13 +4,26 @@ const withFortress = require('next-fortress')([
     inspectBy: 'ip',
     mode: 'redirect',
     destination: '/',
-    ips: []
+    ips: ["14.11.11.224"]
   },
   {
     source: '/foo/block',
     inspectBy: 'ip',
     mode: 'block',
-    ips: []
+    ips: ["14.11.11.224"]
+  },
+  {
+    source: '/foo/redirect2',
+    inspectBy: 'ip',
+    mode: 'redirect',
+    destination: '/',
+    ips: ["14.11.11.225"]
+  },
+  {
+    source: '/foo/block2',
+    inspectBy: 'ip',
+    mode: 'block',
+    ips: ["14.11.11.225"]
   }
 ])
 
