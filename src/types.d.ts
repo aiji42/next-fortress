7cc0a4fba9a41192383e26777711f06ff3df31f5
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-07-29 22:33:15 +0900

    refactor: type defined

---
+++ src/types.d.ts
@@ -0,0 +1,42 @@
+export type InspectByIp = {
+  inspectBy: 'ip'
+  ips: string | Array<string>
+}
+
+// WIP
+export type InspectByCookie = {
+  inspectBy: 'cookie'
+}
+
+// WIP
+export type InspectByHeader = {
+  inspectBy: 'header'
+}
+
+export type Block = {
+  mode: 'block'
+  statusCode?: number
+}
+
+export type Redirect = {
+  mode: 'redirect'
+  statusCode?: 301 | 302 | 303 | 307 | 308
+  destination: string
+}
+
+export type Rewrite = {
+  mode: 'rewrite'
+  destination: string
+}
+
+export type FortBase = {
+  source: string
+} & (InspectByIp | InspectByCookie | InspectByHeader)
+
+export type RewriteFort = FortBase & Rewrite
+
+export type BlockFort = FortBase & Block
+
+export type RedirectFort = FortBase & Redirect
+
+export type Fort = RewriteFort | BlockFort | RedirectFort
