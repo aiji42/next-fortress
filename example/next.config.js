5f13120993dcd33b79a11e69dd35c95baa574975
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-07-29 22:35:53 +0900

    chore: update example

--- example/next.config.js
+++ example/next.config.js
@@ -8,12 +8,6 @@ const withFortress = require('next-fortress')({
       ips: ["14.11.11.224"]
     },
     {
-      source: '/foo/block',
-      inspectBy: 'ip',
-      mode: 'block',
-      ips: ["14.11.11.224"]
-    },
-    {
       source: '/foo/redirect2',
       inspectBy: 'ip',
       mode: 'redirect',
@@ -21,15 +15,39 @@ const withFortress = require('next-fortress')({
       ips: ["14.11.11.225"]
     },
     {
+      source: '/foo/block',
+      inspectBy: 'ip',
+      mode: 'block',
+      ips: ["14.11.11.224"]
+    },
+    {
       source: '/foo/block2',
       inspectBy: 'ip',
       mode: 'block',
+      ips: ["14.11.11.225"]
+    },
+    {
+      source: '/foo/rewrite',
+      inspectBy: 'ip',
+      mode: 'rewrite',
+      destination: '/foo/rewritten',
+      ips: ["14.11.11.224"]
+    },
+    {
+      source: '/foo/rewrite2',
+      inspectBy: 'ip',
+      mode: 'rewrite',
+      destination: '/foo/rewritten2',
       ips: ["14.11.11.225"]
     }
   ],
   host: process.env.VERCEL_URL ?? 'http://localhost:3000'
 })
 
-module.exports = withFortress({
+const config = withFortress({
   reactStrictMode: true,
 })
+
+config.rewrites?.().then(console.log)
+
+module.exports = config
