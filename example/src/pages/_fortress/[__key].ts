9fd1da4a7adebc0d7d83eafa91d89872363cd305
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-08-03 23:52:14 +0900

    chore: debug on vercel

--- example/src/pages/_fortress/[__key].ts
+++ example/src/pages/_fortress/[__key].ts
@@ -1,7 +1,26 @@
-import { GetServerSideProps } from 'next'
+import {GetServerSideProps, GetServerSidePropsContext} from 'next'
 import { getServerSideProps as _getServerSideProps } from 'next-fortress/build/inspect'
+import { withSSRContext } from 'aws-amplify'
+
+export const verifyCognitoAuthenticatedUser = async (
+  ctx: GetServerSidePropsContext
+): Promise<boolean> => {
+  const { Auth } = withSSRContext(ctx)
+  console.log(ctx.req.cookies)
+  let authenticated: boolean
+  try {
+    const user = await Auth.currentAuthenticatedUser()
+    authenticated = !!user
+  } catch (e) {
+    console.log(e)
+    authenticated = false
+  }
+
+  return authenticated
+}
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  await verifyCognitoAuthenticatedUser(ctx)
   return await _getServerSideProps(ctx)
 }
 
