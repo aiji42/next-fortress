b79e155c3715f4bd89a35298fd81db2f6692621f
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-11-06 15:09:35 +0900

    chore: update example

--- example/next.config.js
+++ example/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     AUTH0_BASE_URL:
-      process.env.AUTH0_BASE_URL ??
+      process.env.AUTH0_BASE_URL ||
       (process.env.VERCEL_URL
         ? `https://${process.env.VERCEL_URL}`
         : 'http://localhost:3000')
